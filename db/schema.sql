95e15a5627096e7c3409ad61182ba435e076df63
TMSulik <shulich@hotmail.com>
2019-04-01 21:21:48 -0700

    Sign in modal

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,13 @@ CREATE DATABASE atlas_db;
 
 USE atlas_db;
 
--- Create a burgers table with the required fields --
+-- Create a users table with the required fields --
 CREATE TABLE users
 (
 	id int NOT NULL AUTO_INCREMENT,
 	username VARCHAR(255) NOT NULL,
 	pw VARCHAR(255) NOT NULL,
-    categories INT,
-    watchlist INT,
-  	PRIMARY KEY(id)
+  categories INT,
+  watchlist INT,
+  PRIMARY KEY(id)
 );
