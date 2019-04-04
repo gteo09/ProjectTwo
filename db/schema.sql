3c10587f6b0bd9ddc474ca3b5982fec9f688f4ea
Ethanmo <mythface00@gmail.com>
2019-04-03 20:26:56 -0700

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,18 @@ USE atlas_db;
 -- Create a burgers table with the required fields --
 CREATE TABLE users
 (
-	id int NOT NULL AUTO_INCREMENT,
+	userId int NOT NULL AUTO_INCREMENT,
 	username VARCHAR(255) NOT NULL,
-	pw VARCHAR(255) NOT NULL,
-    categories INT,
-    watchlist INT,
-  	PRIMARY KEY(id)
+	'password' VARCHAR(255) NOT NULL,
+  PRIMARY KEY(userId),
+	CONSTRAINT watch_lists FOREIGN KEY (userId)
+	REFERENCES users(userId);
+);
+
+CREATE TABLE watch_lists
+(
+	id int NOT NULL PRIMARY KEY,
+	list_name VARCHAR(50) NOT NULL,
+	movies JSON,
+	userId INT FOREIGN KEY REFERENCES users(userId)
 );
