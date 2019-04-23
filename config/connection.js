6e95df23137e26d4020511693514edb0ea955079
gteo09 <44217145+gteo09@users.noreply.github.com>
2019-04-22 21:53:43 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -23,7 +23,7 @@ var connection = mysql.createConnection({
   host:     'localhost',
   port:     3306,
   user:     'root',
-  password: process.env.mysqlpw,
+  password: "",
   database: 'atlas_db'
 })
 
